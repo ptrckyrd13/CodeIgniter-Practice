276462248574e3c62b235d7d003540872dd80de2
Patrick Oyardo <patpat.oyardo@users.noreply.github.com>
2019-08-18 19:56:56 +0800

    first commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<title>ciBlog</title>
+		<link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container">
+  	<a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+      </li>
+          <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</div>
+</nav>
+
+  <div class="container">
